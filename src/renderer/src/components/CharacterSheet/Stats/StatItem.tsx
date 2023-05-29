94c304b12536285383ce24f8242ff516a1666eb3
Matthew M <mattemoye@gmail.com>
2023-05-29 18:28:19 -0400

    update stat item, and update info section inputs

--- src/renderer/src/components/CharacterSheet/Stats/StatItem.tsx
+++ src/renderer/src/components/CharacterSheet/Stats/StatItem.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { abilityScoreModifierFormula } from "@renderer/templates/pf1e/Info";
 import classes from "./StatItem.module.css";
 
 type StatItemProps = {
@@ -6,12 +8,21 @@ type StatItemProps = {
 
 const StatItem = (props: StatItemProps) => {
   const { stat } = props;
+
+  const [abilityScore, setAbilityScore] = useState(12);
+  const mod = abilityScoreModifierFormula(abilityScore);
+  console.log(abilityScoreModifierFormula(0));
+
   return (
     <div className={classes.field}>
-      <label className={classes.label}>
-        <input className={classes.input} />
-        {stat}
-      </label>
+      <label className={classes.label}>{stat}</label>
+      <input
+        type={"number"}
+        value={abilityScore}
+        onChange={(e) => setAbilityScore(Number(e.target.value))}
+        className={classes.input}
+      />
+      <span className={classes.statMod}>{mod}</span>
     </div>
   );
 };
--- src/renderer/src/templates/pf1e/Info.ts
+++ src/renderer/src/templates/pf1e/Info.ts
@@ -27,3 +27,13 @@ export const tabsFields = {
     misc: "",
   },
 };
+
+export const extraSpellFormula = (spellLvl: number, abilityScore: number) => {
+  if (spellLvl === 0) return 0;
+  return Math.round((abilityScore - spellLvl) / 4 + 0.5);
+};
+
+export const abilityScoreModifierFormula = (ability: number) => {
+  const minNum = ability % 2 === 0 ? ability : ability - 1;
+  return minNum - (5 + Math.floor(minNum / 2));
+};
